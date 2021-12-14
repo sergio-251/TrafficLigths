df66ea50b67248db3854b57374eee34bccbfffeb
EfimovSchool <sergio-251@yandex.ru>
2021-12-14 15:25:14 +0300

    Initial Commit

---
+++ TrafficLigths/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLigths
+//
+//  Created by Sergey Efimov on 14.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
